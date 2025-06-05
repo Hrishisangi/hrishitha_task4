c24109d4fc0a6654dc110405aed0b23bd61d4ed3
Hrishisangi <130072987+Hrishisangi@users.noreply.github.com>
2025-06-05 14:28:13 +0530

    changed path

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
+import Login from './pages/Login.js';
+import Signup from './pages/Signup.js';
+import Home from './pages/Home.js';
 import { useState } from 'react';
 import RefrshHandler from './RefreshHandler';
 
